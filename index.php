b1277d7aca1f1b6b0c7fcfe46ac81412077db686
Sultan Baharuddin <50097679+kamikira-senpai@users.noreply.github.com>
2020-04-24 05:46:38 +0800

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ $baseurl = 'http://localhost/cURL-with-web-interface/';
 				
 					<div class="input-group mb-3">
 					  	<div class="input-group-prepend">
-					    	<span class="input-group-text" id="inputGroup-sizing-default">Link :</span>
+					    	<span class="input-group-text" id="inputGroup-sizing-default">Url :</span>
 					  	</div>
 			  		<input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" name="url">
 					</div>
